49370d924154907bf6bed0d4626115878ff119b5
danielsychoo <sungyeop.choo@gmail.com>
2022-04-11 09:19:26 +0900

    [Fix] Update AutoLayoutExample numbering

--- 1-1-AutoLayoutExample/1-AutoLayoutExample/ViewController.swift
+++ 1-1-AutoLayoutExample/1-AutoLayoutExample/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func tabChangeColorButton(_ sender: UIButton) {
+        self.colorView.backgroundColor = UIColor.blue
+        print("버튼이 클릭 됨")
+    }
 }
 
--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ```planetext
 study-swift -- [repository]
   ┣ 0-swift-grammar.playground -- [0. Swift 문법 공부]
-  ┗ 1-AutoLayoutExample -- [1. AutoLayout 실습 project]
+  ┗ 1-1-AutoLayoutExample -- [1-1. AutoLayout 실습]
 ```
