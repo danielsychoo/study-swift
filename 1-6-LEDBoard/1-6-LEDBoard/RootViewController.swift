4b34f313816a3ed43ea424995d5c85f64d2b0a00
danielsychoo <sungyeop.choo@gmail.com>
2022-04-20 09:46:45 +0900

    [Update] Add UI on LEDBoard

--- 1-6-LEDBoard/1-6-LEDBoard/RootViewController.swift
+++ 1-6-LEDBoard/1-6-LEDBoard/RootViewController.swift
@@ -13,7 +13,6 @@ class RootViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
---
+++ 1-6-LEDBoard/1-6-LEDBoard/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  1-6-LEDBoard
+//
+//  Created by sungyeopTW on 2022/04/20.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
